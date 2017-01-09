ee1b26d10035210f5f3adb9c26744f7818be4885
David Quisenberry and philip putnam <david@>
2017-01-09 10:31:35 -0800

    add basic html, js for cooking conversions

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+// Cooking Application
+
+// tablespoon to teaspoon
+
+var tbspToTsp = function(units) {
+  var tsp = units * 3;
+  return tsp;
+}
+
+// teaspoon to tablespoon
+var tspToTbsp = function(units) {
+  var tbsp = units / 3;
+  return tbsp;
+}
+
+// cup to tablespoon
+var cupToTbsp = function(units) {
+  var tbsp = units * 16;
+  return tbsp;
+}
+// tablespoon to cup
+var tbspToCup = function(units) {
+  var cup = units / 16;
+  return cup;
+}
+// cup to teaspoon
+var cupToTsp = function(units) {
+  var tsp = units * 48;
+  return tsp;
+}
+// teaspoon to cup
+var tspToCup = function(units) {
+  var cup = units / 48;
+  return cup;
+}
+// cup to fluid ounces
+var cupToFl = function(units) {
+  var fl = units * 8;
+  return fl;
+}
+// cup to pints
+var cupToPint = function(units) {
+  var pint = units / 2;
+  return pint;
+}
+// pints to cups
+var pintsToCup = function(units) {
+  var cup = units * 2;
+  return cup;
+}
+// pints to quarts
+var pintToQuart = function(units) {
+  var quarts = units / 2;
+  return quarts;
+}
+// quarts to pints
+var quartToPint = function(units) {
+  var pints = units * 2;
+  return pints;
+}
+// cups to quarts
+var cupsToQuarts = function(units) {
+  var quarts = units / 4;
+  return quarts;
+}
+// quarts to cups
+var quartToCup = function(units) {
+  var cup = units * 4;
+  return cup;
+}
+// quarts to gallons
+var quartsToGallon = function(units) {
+  var gallons = units / 4;
+  return gallons;
+}
+// gallons to quarts
+var gallonsToQuart = function(units) {
+  var quarts = units * 4;
+  return quarts;
+}
+// ounces to pounds
+var ozToLbs = function(units) {
+  var pounds = units / 16;
+  return pounds;
+}
+// pounds to ounces
+var lbsToOz = function(units) {
+  var ounces = units * 16;
+  return ounces;
+}
